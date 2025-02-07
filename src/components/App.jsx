2c69a945888633a3270d4d93d9b4fc60b71b84b2
Svitlana Markevych <svetlana2507t@gmail.com>
2025-02-07 10:04:21 +0100

    create project

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import s from './App.module.css';
+
+//import {Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <main>
+      <h1 className={s.header}>Vite + React</h1>
+      {/*  <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes> */}
+    </main>
+  );
+}
+
+export default App;
